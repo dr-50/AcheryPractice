2bfba94e782e58077477c795189d00f1c73f5f25
Devon <devonross50@yahoo.com>
2023-05-22 13:26:13 -0500

    updated logic for missing values in input fields

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 
 function targetDistance(min,max,numTarget){
+    console.log('called')
+    min=parseInt(document.getElementById('minDistance').value, 10);
+    max=parseInt(document.getElementById('maxDistance').value, 10);
+    numTarget=parseInt(document.getElementById('numTargets').value, 10);
+    yardArray = []
+    console.log(min)
+    console.log(max)
+    console.log(numTarget)
+
+//Check if input fields contain values -- call missingFields function of one field is missing 
+    if(document.getElementById('maxDistance').value=='' || document.getElementById('numTargets').value == ''){
+        missingFields()
+    } else {
+        resetFieldsMissing()
+    }
+//Check if generated yard list already exists on the screen -- call clearYards if true
     yardList = document.getElementById('yardList')
     if(yardList!=null){
         clearYards()
     }
 
-    yardArray = []
-    min=parseInt(document.getElementById('minDistance').value, 10);
-    max=parseInt(document.getElementById('maxDistance').value, 10);
-    numTarget=parseInt(document.getElementById('numTargets').value, 10);
+//Generate random numbers and push to array
     for (let i=0; i<numTarget; i++)
     {
         num= Math.random()*(max-min)+min;
         yardArray.push(num.toFixed(1))
     }
-
+//append random number arry to Ordered List nodes 
     list=document.getElementById('yardList');
     for (i=0; i<yardArray.length; i++){
         var li = document.createElement('li');
@@ -23,8 +36,26 @@ function targetDistance(min,max,numTarget){
     }
 }
 
-
+//remove existing generated yard list
 function clearYards(){
     yardList = document.getElementById('yardList')
     yardList.innerHTML = '';
+}
+
+//Format input fields to indicate missing value
+function missingFields(){
+    alert('please enter all required fields')
+    if(document.getElementById('maxDistance').value==''){
+        document.getElementById('maxDistance').className = document.getElementById('maxDistance').className + 'error'   
+    }
+    if(document.getElementById('numTargets').value==''){
+        document.getElementById('numTargets').className = document.getElementById('numTargets').className + 'error'     
+    }
+}
+
+//Reset input fields to default formatting 
+function resetFieldsMissing(){
+    document.getElementById('maxDistance').className = document.getElementById('maxDistance').className.replace('error', '')
+    document.getElementById('numTargets').className = document.getElementById('numTargets').className.replace('error', '')     
+
 }
